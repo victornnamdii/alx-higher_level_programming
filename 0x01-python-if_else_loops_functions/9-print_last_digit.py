aaf1345978a2f7bfad467b780132ab16cb8be19b
“victornnamdii” <victorilodiuba@gmail.com>
2022-06-28 14:32:01 -0700

    Edit: 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return((-1 * number) % 10)
-    else:
-        return(number % 10)
+        number = (-1 * number)
+    print("{}".format((number % 10)), end='')
+    return(number % 10)
