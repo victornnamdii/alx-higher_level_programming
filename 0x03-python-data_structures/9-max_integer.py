d6acefcc051b5bec58a02bfca879c388d98aaa24
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-02 03:45:51 -0700

    Add: 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maxn = my_list[0]
+        for i in my_list:
+            if i > maxn:
+                maxn = i
+        return(maxn)
+    return(None)
