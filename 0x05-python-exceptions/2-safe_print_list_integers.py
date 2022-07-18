8a5f5b62db2cd87163e52e1d99aa51db9afee057
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-18 12:09:12 -0700

    Add: 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            n += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return n
