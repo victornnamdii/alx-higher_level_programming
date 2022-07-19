14d3ea4b92171b4268c11f0efb3c265324c45d52
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-19 15:40:40 -0700

    Add: 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a class called square"""
+
+
+class Square:
+    """Initialize a square
+    Args:
+        __size (int): size of the square
+    """
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size of the square
+        Returns: None
+        """
+        self.__size = size
