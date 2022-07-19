a3695530910ca006ff336e544899bd00e375d746
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-19 15:54:27 -0700

    Edit(1): 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,7 +15,8 @@ class Square:
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
