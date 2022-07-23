c787919b8bac3faa0717588e685a61c27c14a162
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-23 11:27:46 -0700

    Add: 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+File: 0-add_integer.py
+Desc: This module supplies one function, add_integer(a, b)
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: Jul 23 2022
+"""
+
+def add_integer(a, b=98):
+    """
+    This function computes the addition of two numbers
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
