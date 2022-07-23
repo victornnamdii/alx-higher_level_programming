d7cb3c664f35bde325761f707a0b8721b11acbbb
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-23 13:59:17 -0700

    Add: 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+File: 4-print_square.py
+Desc: This module contains one function: print_square(size)
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 23 Jul 2022
+"""
+
+
+def print_square(size):
+    """
+    This function prints a square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
