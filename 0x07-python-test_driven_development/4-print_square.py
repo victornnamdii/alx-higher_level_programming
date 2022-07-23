52eedc0bfc2e27bbf45082516070e5cbf6a28b71
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-23 14:03:50 -0700

    Edit(1): 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
     """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if type(size) < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
