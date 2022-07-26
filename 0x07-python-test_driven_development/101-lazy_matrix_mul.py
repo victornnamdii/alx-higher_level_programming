29c14c08c36341865e902667478ad1cc52e05b62
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-26 14:42:43 -0700

    Add: 101

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+File: 101-lazy_matrix_mul.py
+Desc: This module contains one function: lazy_matrix_mul(m_a, m_b)
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 26 Jul 2022
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    computes matrix multiplication
+    """
+    return numpy.matmul(m_a, m_b)
