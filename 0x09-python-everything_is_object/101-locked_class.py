c5fa76ba4acb093c0da87521e3142ec6503f3b50
Ilodiuba Victor <101352050+victornnamdii@users.noreply.github.com>
2022-07-26 22:26:07 +0100

    Edit(4): 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -13,9 +13,4 @@ class LockedClass:
     instance attributes, except if the new instance attribute
     is called first_name
     """
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '"
-                                 + name + "'")
+    __slots__ = ["first_name"]
