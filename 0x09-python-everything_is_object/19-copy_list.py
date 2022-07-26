79abc327e8b668fb18bec88844289d1211988ca2
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-26 10:51:10 -0700

    Edit(1): 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-"""
-File: 19-copy_list.py
-Desc: This module contains one function, copy_list(l)
-Author: Ilodiuba Victor (victornnamdii)
-Date Created: 26 Jul 2022
-"""
-
-
 def copy_list(l):
-    """
-    This function returns the copy of a list
-    """
     return l[:]
