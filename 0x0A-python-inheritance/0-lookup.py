bd5172388564452589db59b771688a0fed68902c
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-01 15:53:31 -0700

    Add: 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+File: 0-lookup.py
+Desc: This module contains one function; lookup(obj)
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 01 Aug 2022
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and
+    methods of an object
+    """
+    return dir(obj)
