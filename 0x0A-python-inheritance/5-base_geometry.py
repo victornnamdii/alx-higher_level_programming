9a0033e21f6fa8a0fd991e79fda6fe4329ca5ae4
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-01 16:56:36 -0700

    Add: 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+File: 5-base_geometry.py
+Desc: This module contains an empty class; BaseGeometry
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 02 Aug 2022
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class
+    """
+    pass
