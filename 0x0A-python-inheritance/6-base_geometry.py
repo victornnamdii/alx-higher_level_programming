f9068df95d71584edb13df585557bce8fc8a01a2
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-01 17:01:10 -0700

    Add: 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+File: 6-base_geometry.py
+Desc: This module contains a class; BaseGeometry
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 02 Aug 2022
+"""
+
+
+class BaseGeometry:
+    """
+    Representation of the class
+    """
+    def area(self):
+        """
+        Raises an exception
+        """
+        raise Exception("area() is not implemented")
