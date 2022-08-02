e57483d7cc403ed33479b0b1dece803116e9cfb7
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-02 14:29:59 -0700

    Add: 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+File: 9-student.py
+Desc: This module contains one class; Student
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 02 Aug 2022
+"""
+
+
+class Student:
+    """
+    Reoresentation of the class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return ({key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)})
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
