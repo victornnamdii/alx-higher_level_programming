4400b24f94eacab9939708ba1c4c703e69827b46
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-02 13:53:38 -0700

    Add: 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+File: 9-student.py
+Desc: This module contains one class; Student
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 02 Aug 2022
+"""
+
+
+class Student:
+    """
+    Reoresentation of the class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
