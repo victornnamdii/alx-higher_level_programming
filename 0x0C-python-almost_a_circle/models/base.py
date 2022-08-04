143cca54f1e502f445e3d41990977c578d607742
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-04 04:20:28 -0700

    Add: models/base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+File: base.py
+Desc: THis module contains a class; Base
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 04 Aug 2022
+"""
+
+
+class Base:
+    """
+    Representation of the class Base
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialization of the class
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+File: test_base.py
+Desc: This module contain unittest for models/base.py
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 04 Aug 2022
+"""
+
+import unittest
+Base = __import__('models/base').Base
+
+class TestBase(unittest.TestCase):
+    """
+    Unit tests for the base model
+    """
+
+    def test_id_init(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(70)
+        b4 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 70)
+        self.assertEqual(b4.id, 3)
+
+if __name__ == '__main__':
+    unittest.main()
