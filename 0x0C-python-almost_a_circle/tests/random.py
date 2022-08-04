17587463b4caa3cf217b2eecd113b7739c3aea42
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-04 03:41:29 -0700

    Add: random test file

---
+++ 0x0C-python-almost_a_circle/tests/random.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+File: random.py
+Desc: This module contains a random unittest
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 04 Aug 2022
+"""
+
+import unittest
+
+
+class RandomTest(unittest.TestCase):
+    """
+    a random test
+    """
+    def test_random(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
