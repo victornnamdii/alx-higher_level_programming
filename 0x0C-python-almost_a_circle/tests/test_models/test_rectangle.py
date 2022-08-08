88ed8d1d4dd42acf14fb69ed30002550d4765177
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-08 14:54:48 -0700

    Edit tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,7 +6,6 @@ Author: Ilodiuba Victor (victornnamdii)
 Date Created: 04 Aug 2022
 """
 
-import pep8
 import inspect
 from io import StringIO
 import unittest
@@ -14,41 +13,6 @@ from unittest.mock import patch
 from models import rectangle
 from models.base import Base
 Rectangle = rectangle.Rectangle
-
-
-class TestRectangleDocs(unittest.TestCase):
-    """Tests the Rectangle class' style and documentation"""
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.rect_funcs = inspect.getmembers(Rectangle, inspect.isfunction)
-
-    def test_pep8_conformance_rectangle(self):
-        """Test that models/rectangle.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/rectangle.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_rectangle(self):
-        """Test that tests/test_models/test_rectangle.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_rectangle.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_module_docstring(self):
-        """Tests for the presence of a module docstring"""
-        self.assertTrue(len(rectangle.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """Tests for the presence of a class docstring"""
-        self.assertTrue(len(Rectangle.__doc__) >= 1)
-
-    def test_func_docstrings(self):
-        """Tests for the presence of docstrings in all functions"""
-        for func in self.rect_funcs:
-            self.assertTrue(len(func[1].__doc__) >= 1)
 
 
 class TestRectangle(unittest.TestCase):
