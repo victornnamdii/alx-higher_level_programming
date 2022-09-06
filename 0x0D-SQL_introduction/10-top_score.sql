d01976a9c7437b3a095d4b85b67c9eb9b2280e57
“victornnamdii” <victorilodiuba@gmail.com>
2022-09-06 15:32:13 -0700

    Add: 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- script that lists all records of the table second_table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
