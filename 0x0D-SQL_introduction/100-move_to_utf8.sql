7e31711b3f717a6c4143153e56871886832b35aa
Ilodiuba Victor <101352050+victornnamdii@users.noreply.github.com>
2022-09-07 05:08:21 +0100

    Add: 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,17 @@
+-- Convert db 'hbtn_0c_0'
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+-- Convert table 'first_table'
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Convert field 'name' in 'first_table'
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CHANGE name name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
