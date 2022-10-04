f9fc43b20b3b463f5769ff4d9ed163cb37a0944a
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-04 16:14:49 +0100

    Edit: 100-map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 const list = require('./100-data').list;
 
 console.log(list);
-const list2 = list.map(x => x * list.indexOf(x));
+const list2 = list.map((x, index) => x * index);
 console.log(list2);
