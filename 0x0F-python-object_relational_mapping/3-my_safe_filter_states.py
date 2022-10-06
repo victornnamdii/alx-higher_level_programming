d0614fe42c0507b176801b8e76d8eb95d77a99b4
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-06 23:14:36 +0100

    Add: 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+File: 3-my_safe_filter_states.py
+Author: Ildoiuba Victor
+Desc: a script that takes in an argument and displays all values in the states
+        table of hbtn_0e_0_usa where name matches the argument but safe from
+        injections.
+Date: 06 Oct 2022
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
