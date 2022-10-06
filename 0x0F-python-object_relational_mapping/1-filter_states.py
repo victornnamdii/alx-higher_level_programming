7603462efc8d17d1d389dbc90c9e7e0f3b674e32
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-06 22:03:22 +0100

    Add: 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+File: 0-select_states.py
+Author: Ildoiuba Victor
+Desc: a script that lists all states from the database hbtn_0e_0_usa
+Date: 06 Oct 2022
+"""
+
+import MySQLdb
+from sys import argv
+
+
+db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                     passwd=argv[2], db=argv[3])
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+db.close()
