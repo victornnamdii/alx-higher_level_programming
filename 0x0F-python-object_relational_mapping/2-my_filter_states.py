638d5bb8cb7f6291458ad92e6882094b6db1731e
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-06 22:17:54 +0100

    Add: 2 and Edit: 1 & 0

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-File: 1-filter_states.py
+File: 2-my_filter_states.py
 Author: Ildoiuba Victor
-Desc: a script that lists all states with a name starting with N
-        from the database hbtn_0e_0_usa.
+Desc: a script that takes in an argument and displays all values in the states
+        table of hbtn_0e_0_usa where name matches the argument.
 Date: 06 Oct 2022
 """
 
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' \
+                    ORDER BY id ASC".format(argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
