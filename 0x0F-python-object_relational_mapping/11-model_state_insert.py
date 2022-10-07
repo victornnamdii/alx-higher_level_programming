9aa608fe6708d4e540240045c45702a2cfba592e
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-07 22:33:56 +0100

    Add: 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+File: 7-model_state_fetch_all.py
+Author: Ildoiuba Victor
+Desc: a script that lists all State objects from the
+        database hbtn_0e_6_usa
+Date: 07 Oct 2022
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    nstate = State(name='Louisiana')
+    session.add(nstate)
+    session.commit()
+    print(nstate.id)
+    session.close()
