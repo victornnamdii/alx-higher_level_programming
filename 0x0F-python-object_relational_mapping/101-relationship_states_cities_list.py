589bb5332e0edd57e23f08db396115d715d838dc
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-08 10:10:35 +0100

    Edit(2);101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -2,8 +2,6 @@
 """
     a script that lists all State objects, and corresponding
     City objects, contained in the database hbtn_0e_101_usa
-
-    Author: Ilodiuba Victor (victornnamdii)
 """
 
 from sqlalchemy import create_engine
@@ -18,7 +16,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).join(City).order_by(State.id, City.id).all()
+    states = session.query(State).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
