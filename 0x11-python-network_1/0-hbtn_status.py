c48fb1a4675bcfad1785d15b5c816b5402db88a8
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-21 22:14:27 +0100

    Add: 2-10 and edit(0)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except:
+        print("Not a valid JSON")
