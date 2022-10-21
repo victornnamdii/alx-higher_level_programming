102c639d9135796bfaa93c3bba53199e67a4e8e7
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-21 22:27:02 +0100

    Edit(2): 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""takes your Github credentials (username and password) and uses the Github API
+"""takes your Github credentials (username and password)
+and uses the Github API
 to display your id"""
 from sys import argv
 from requests import get, auth
