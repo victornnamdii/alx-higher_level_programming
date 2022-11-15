2eb5f4477fdfdd82e3c7c9a0ff43398fc9954f37
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-15 16:18:31 +0100

    Add: 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filename, content, 'utf-8', err => {
+  if (err) console.log(err);
+});
