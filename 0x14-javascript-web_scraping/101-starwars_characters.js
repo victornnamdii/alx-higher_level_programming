0af58238da74fed48bc946f54726242ef65c803a
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-15 21:47:54 +0100

    Add: 101

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/local/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, async function (error, response, body) {
+  if (!error) display(JSON.parse(body).characters, 0);
+});
+
+function display (characters, i) {
+  request(characters[i], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < characters.length) display(characters, i + 1);
+    }
+  });
+}
