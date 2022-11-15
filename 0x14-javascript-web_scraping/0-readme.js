e71cc01539579dfd5526606450e0bbdebb223c17
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-15 16:05:31 +0100

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) console.log(err);
-	else console.log(data);
+  if (err) console.log(err);
+  else console.log(data);
 });
